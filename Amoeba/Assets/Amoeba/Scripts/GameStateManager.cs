ed262368490c3c60592ac1062192ab505ab5e886
Jezzalittle <jezzalittle20@gmail.com>
2017-09-30 19:35:48 +1000

    CameraRig updated

--- Amoeba/Assets/Amoeba/Scripts/GameStateManager.cs
+++ Amoeba/Assets/Amoeba/Scripts/GameStateManager.cs
@@ -21,22 +21,32 @@ public class GameStateManager : MonoBehaviour {
     private GameObject playerPurplePrefab;
 
 
+
+
     [HideInInspector]
     public int playerCount = 1;
 
     private int maxPlayerCount = 1;
 
+    private List<GameObject> players = new List<GameObject>();
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
         maxPlayerCount = InputManager.Devices.Count + 1;
-
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        //TEMP
+        if(players.Count == 0)
+        {
+            players.Add(GameObject.FindGameObjectWithTag("PlayerRed"));
+        }
+
 	    if(Input.GetKey("b"))
         {
             if(Input.GetKey("u"))
@@ -58,22 +68,34 @@ public class GameStateManager : MonoBehaviour {
                     playerCount++;
                     if (playerCount == 1)
                     {
-                        Instantiate(playerRedPrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity);
+                        players.Add(Instantiate(playerRedPrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity));
                     }
                     else if (playerCount == 2)
                     {
-                        Instantiate(playerBluePrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity);
+                        players.Add(Instantiate(playerBluePrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity));
                     }
                     else if (playerCount == 3)
                     {
-                        Instantiate(playerYellowPrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity);
+                        players.Add(Instantiate(playerYellowPrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity));
                     }
                     else
                     {
-                        Instantiate(playerPurplePrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity);
+                        players.Add(Instantiate(playerPurplePrefab, new Vector3(7f, 47.376f, -131.3f), Quaternion.identity));
                     }
                 }
             }
         }	
 	}
+
+
+    public List<GameObject> Players
+    {
+
+        get
+        {
+            return players;
+        }
+
+    }
+
 }
