776e45d21a299d7ab5103651e01d0c346ee35e94
lman888 <30894807+lman888@users.noreply.github.com>
2017-10-02 17:26:03 +1100

    Started creating the Power Ups as well as the Spawners for them.

---
+++ Amoeba/Assets/Amoeba/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+    //Allows the GameObject to be seen in the inspector
+    [SerializeField]
+    private GameObject[] powerUps;
+
+    [SerializeField]
+    private float spawnCoolDown;
+
+    [SerializeField]
+    private GameObject[] spawnPointPrefab;
+
+    private float powerUpSpawnTimer;
+
+    private GameObject currentPowerUp;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //Creates a timer that counts up
+        powerUpSpawnTimer += Time.deltaTime;
+
+        if (powerUpSpawnTimer > spawnCoolDown)
+        {
+            powerUpSpawnTimer = 0.0f;
+
+            //For Each GameObject in the spawnPointPrefab
+            foreach (GameObject i in spawnPointPrefab)
+            {
+                if (i.transform.GetChild(0) == null)
+                {
+                    //Creates a new randomised Power Up gameObject and sets it to a random SpawnPoint
+                    currentPowerUp = Instantiate(powerUps[(int)Random.Range(0, powerUps.Length - 1)], i.transform.position + (transform.up * 2), Quaternion.identity);
+                    currentPowerUp.transform.SetParent(i.transform);
+                }
+            }
+        }
+	}
+}
