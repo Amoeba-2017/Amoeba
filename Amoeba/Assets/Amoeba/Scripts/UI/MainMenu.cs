565bbf8a182115d5112ed3732f98d9919ece823c
SirGhostler <eljosho1998@gmail.com>
2017-10-09 14:02:29 +1100

    Main Menu progress, can now start prototype_Scene_001.

--- Amoeba/Assets/Amoeba/Scripts/UI/MainMenu.cs
+++ Amoeba/Assets/Amoeba/Scripts/UI/MainMenu.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+        {
+          Application.LoadLevel(sceneName);
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void QuitApplication()
+      {
+        Application.Quit();
+      }
 }
--- Amoeba/Assets/InControl/Source/VersionInfo.cs
+++ Amoeba/Assets/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-				Build = 4166
+				Build = 4181
 			};
 		}
 
