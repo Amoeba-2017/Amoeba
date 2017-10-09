feaeb12106ed4d2285d6001ad16315f0c8cc0f6f
Jezzalittle <jezzalittle20@gmail.com>
2017-10-10 09:48:40 +1100

    playerSelect

---
+++ Amoeba/Assets/Amoeba/Scripts/UserInterfaceManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterfaceManager : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas mainMenu;
+
+    [SerializeField]
+    private Canvas playerSelect;
+
+    
+    //called when the play button is pressed on the main menu
+    void PlayButton()
+    {
+
+    }
+
+
+}
