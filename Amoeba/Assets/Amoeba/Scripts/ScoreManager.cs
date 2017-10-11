0c25d52ad2d57844427cb99ca720acbfafe78cb8
SirGhostler <eljosho1998@gmail.com>
2017-10-11 12:22:51 +1100

    Beginning to code ScoreManager, within prototype_Scene_001.

---
+++ Amoeba/Assets/Amoeba/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int roundsWon; // Player round wins
+
+    private GameStateManager gsm;
+
+    // Initialization
+    void Start ()
+    {	
+	}
+	
+	// Update (Per-Frame)
+	void Update ()
+    {	
+	}
+}
