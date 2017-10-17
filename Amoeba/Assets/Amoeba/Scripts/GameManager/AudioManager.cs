0664f628beefce9f58ea8babaddb9d851a0455e8
SirGhostler <eljosho1998@gmail.com>
2017-10-17 16:01:13 +1100

    Implemented audio code and Audio Manager. To be adjusted later on.

---
+++ Amoeba/Assets/Amoeba/Scripts/GameManager/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioClip ShootSound, DeathSound, VictorySound;
+    static AudioSource am;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ShootSound = Resources.Load<AudioClip>("ShootSound_Placeholder");
+
+        am = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public static void PlaySound (string clip)
+    {
+        switch (clip)
+        {
+            // Slime Shooting sound
+            case "ShootSound_Placeholder": am.PlayOneShot(ShootSound, 1f);
+                break;
+        }
+    }
+}
