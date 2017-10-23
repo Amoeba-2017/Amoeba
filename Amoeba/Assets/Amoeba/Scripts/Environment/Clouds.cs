dc772c487b155316c62d5862cf6177d7e5aa2ed7
SirGhostler <eljosho1998@gmail.com>
2017-10-23 17:09:12 +1100

    Begun work on cloud scripting.

---
+++ Amoeba/Assets/Amoeba/Scripts/Environment/Clouds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+    [SerializeField]
+    private float spawnX;
+    [SerializeField]
+    private float spawnY;
+    [SerializeField]
+    private float spawnZ;
+
+    [SerializeField]
+    private float spawnFrequency;
+    [SerializeField]
+    private float speed;
+
+    //[SerializeField]
+    //private GameObject cloudA;
+    //[SerializeField]
+    //private GameObject cloudB;
+
+    // Initialization
+    void Start ()
+    {
+        transform.Translate(spawnX, spawnY, spawnZ);
+	}
+	
+	// Update (Per Frame)
+	void Update ()
+    {	
+	}
+}
