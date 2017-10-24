98322afee6df5fa41f1694b24002fe5fc2f2254b
SirGhostler <eljosho1998@gmail.com>
2017-10-24 16:42:14 +1100

    Almost done with cloud script.

--- Amoeba/Assets/Amoeba/Scripts/Environment/Clouds.cs
+++ Amoeba/Assets/Amoeba/Scripts/Environment/Clouds.cs
@@ -4,81 +4,50 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour {
 
-    // Spawn Coordinates
-    [SerializeField]
-    [Tooltip("X axis spawn position.")]
-    private float spawnX;
-    [SerializeField]
-    [Tooltip("Y axis spawn position.")]
-    private float spawnY;
-    [SerializeField]
-    [Tooltip("Z axis spawn position.")]
-    private float spawnZ;
-
     // Scaling
     [SerializeField]
     [Tooltip("X axis scale.")]
-    private float scaleX;
+    private float scaleX;       // X axis
     [SerializeField]
     [Tooltip("Y axis scale.")]
-    private float scaleY;
+    private float scaleY;       // Y axis
     [SerializeField]
     [Tooltip("Z axis scale.")]
-    private float scaleZ;
+    private float scaleZ;       // Z axis
 
     // Velocity
     [SerializeField]
     [Tooltip("X axis movement speed.")]
-    private float speedX;
+    private float speedX;       // X axis
     [SerializeField]
     [Tooltip("Y axis movement speed.")]
-    private float speedY;
+    private float speedY;       // Y axis
     [SerializeField]
     [Tooltip("Z axis movement speed.")]
-    private float speedZ;
-
-    [SerializeField]
-    [Tooltip("The set time which a cloud will spawn after the last.")]
-    private float spawnSetTime;
-    private float spawnCurrentTime;
+    private float speedZ;       // Z axis
 
     private Rigidbody rb;
-
-    public Transform cloudPrefab;
+    private Vector3 startPosition;
+    private float distance;
 
     // Initialization
     void Start ()
     {
-        transform.Translate(spawnX, spawnY, spawnZ);
+        startPosition.x = transform.position.x;
+        startPosition.y = transform.position.y;
+        startPosition.z = transform.position.z;
         rb = GetComponent<Rigidbody>();
-        spawnCurrentTime = 0.0f;
+        distance = Vector3.Distance(startPosition, transform.position);
     }
 	
 	// Update (Per Frame)
 	void Update ()
     {
         rb.velocity = new Vector3(speedX, speedY, speedZ);
-        if ((transform.position.z <= 350 || transform.position.z >= -350) || (transform.position.x <= 350 || transform.position.x >= -350))
-        {
-            //DestroyCloud();
-        }
-        spawnCurrentTime += Time.deltaTime;
-        if (spawnCurrentTime == spawnSetTime)
-        {
-            SpawnCloud();
-        }
-    }
 
-    // Spawn
-    void SpawnCloud ()
-    {
-        Instantiate(cloudPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
-        spawnCurrentTime = 0.0f;
-    }
-
-    // Destroy
-    void DestroyCloud ()
-    {
-        Destroy(gameObject);
+        if (distance > 500)
+        {
+            transform.position = startPosition;
+        }
     }
 }
