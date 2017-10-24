68d53bfddf1c7e273f498d4e55f3c03ee9c21910
SirGhostler <eljosho1998@gmail.com>
2017-10-24 11:23:35 +1100

    Updated cloud script. Still needs work.

--- Amoeba/Assets/Amoeba/Scripts/Environment/Clouds.cs
+++ Amoeba/Assets/Amoeba/Scripts/Environment/Clouds.cs
@@ -4,31 +4,81 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour {
 
+    // Spawn Coordinates
     [SerializeField]
+    [Tooltip("X axis spawn position.")]
     private float spawnX;
     [SerializeField]
+    [Tooltip("Y axis spawn position.")]
     private float spawnY;
     [SerializeField]
+    [Tooltip("Z axis spawn position.")]
     private float spawnZ;
 
+    // Scaling
     [SerializeField]
-    private float spawnFrequency;
+    [Tooltip("X axis scale.")]
+    private float scaleX;
     [SerializeField]
-    private float speed;
+    [Tooltip("Y axis scale.")]
+    private float scaleY;
+    [SerializeField]
+    [Tooltip("Z axis scale.")]
+    private float scaleZ;
 
-    //[SerializeField]
-    //private GameObject cloudA;
-    //[SerializeField]
-    //private GameObject cloudB;
+    // Velocity
+    [SerializeField]
+    [Tooltip("X axis movement speed.")]
+    private float speedX;
+    [SerializeField]
+    [Tooltip("Y axis movement speed.")]
+    private float speedY;
+    [SerializeField]
+    [Tooltip("Z axis movement speed.")]
+    private float speedZ;
+
+    [SerializeField]
+    [Tooltip("The set time which a cloud will spawn after the last.")]
+    private float spawnSetTime;
+    private float spawnCurrentTime;
+
+    private Rigidbody rb;
+
+    public Transform cloudPrefab;
 
     // Initialization
     void Start ()
     {
         transform.Translate(spawnX, spawnY, spawnZ);
-	}
+        rb = GetComponent<Rigidbody>();
+        spawnCurrentTime = 0.0f;
+    }
 	
 	// Update (Per Frame)
 	void Update ()
-    {	
-	}
+    {
+        rb.velocity = new Vector3(speedX, speedY, speedZ);
+        if ((transform.position.z <= 350 || transform.position.z >= -350) || (transform.position.x <= 350 || transform.position.x >= -350))
+        {
+            //DestroyCloud();
+        }
+        spawnCurrentTime += Time.deltaTime;
+        if (spawnCurrentTime == spawnSetTime)
+        {
+            SpawnCloud();
+        }
+    }
+
+    // Spawn
+    void SpawnCloud ()
+    {
+        Instantiate(cloudPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+        spawnCurrentTime = 0.0f;
+    }
+
+    // Destroy
+    void DestroyCloud ()
+    {
+        Destroy(gameObject);
+    }
 }
