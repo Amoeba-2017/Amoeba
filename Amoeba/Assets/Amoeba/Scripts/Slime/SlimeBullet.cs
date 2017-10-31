57c0bc16b47d1a3d7dbbcda1351d089a64aa6ca7
Jezzalittle <jezzalittle20@gmail.com>
2017-10-31 22:21:03 +1100

    cleaning up folders

---
+++ Amoeba/Assets/Amoeba/Scripts/Slime/SlimeBullet.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeBullet : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject BulletWallSplat;
+
+    [SerializeField]
+    private GameObject BulletSlimeSplat;
+
+    bool isPickupAble = false;
+
+    Vector3 newRandomPos;
+
+    [SerializeField]
+    private GameObject Puddle;
+
+    float myMass;
+    float hitMass;
+
+
+    public void SetMyMass(float mass)
+    {
+        myMass = mass;
+    }
+
+    public void SetHitMass(float mass)
+    {
+        hitMass = mass;
+    }
+
+    private void CreatePuddles()
+    {
+        if (hitMass > 0 && myMass > 0)
+        {
+            GameObject puddle;
+            puddle = Instantiate(Puddle, transform.position + transform.up, Quaternion.identity);
+            puddle.GetComponent<SlimePuddle>().SetMass(myMass);
+            puddle = Instantiate(Puddle, transform.position + transform.up, Quaternion.identity);
+            puddle.GetComponent<SlimePuddle>().SetMass(hitMass);
+            Debug.Log("puddles Spawned");
+        }
+        else
+        {
+            Debug.Log("0 mass to Throw");
+            Debug.Log(myMass);
+            Debug.Log(hitMass);
+        }
+    }
+
+    void OnCollisionStay(Collision x)
+    {
+        if (x.transform.tag == "Slime")
+        {
+            Debug.Log("trowing mass");
+            CreatePuddles();
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
---
+++ Amoeba/Assets/Amoeba/Scripts/Slime/SlimePuddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimePuddle : MonoBehaviour {
+
+    private float mass;
+
+    Vector3 randomRotDir;
+
+    [SerializeField]
+    private float minRanForce = 2;
+
+    [SerializeField]
+    private float maxRanForce = 5;
+
+    public void SetMass(float a_mass)
+    {
+        mass = a_mass;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        randomRotDir = ((transform.position + transform.up) + new Vector3(Random.Range(1, -1), 0, Random.Range(1, -1))) - transform.position;
+        gameObject.GetComponent<Rigidbody>().AddForce(randomRotDir * Random.Range(minRanForce, maxRanForce), ForceMode.Impulse);
+    }
+	
+
+    void OnCollisionEnter(Collision x)
+    {
+        if (x.transform.tag == "Slime")
+        {
+            GameObject.FindGameObjectWithTag(x.gameObject.GetComponent<SlimeMovement>().parent).GetComponent<PlayerController>().mass += mass;
+            Destroy(gameObject);
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
