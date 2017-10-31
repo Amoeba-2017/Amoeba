156bbd6971ac264ece633089a76828a63a031e52
SirGhostler <eljosho1998@gmail.com>
2017-10-31 15:51:29 +1100

    New Bullet script, testing for Bullet collision particle effects. Uses tags to detect what it collides with (E.g. Rocks with "Rock" tag will cause BulletWallSplat effect.

---
+++ Amoeba/Assets/Amoeba/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject BulletWallSplat;
+
+    [SerializeField]
+    private GameObject BulletSlimeSplat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Rock")
+        {
+            Instantiate(BulletWallSplat, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+        }
+
+        if (col.gameObject.tag == "RedSlime" || col.gameObject.tag == "BlueSlime" || col.gameObject.tag == "YellowSlime" || col.gameObject.tag == "PurpleSlime")
+        {
+            Instantiate(BulletSlimeSplat, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+        }
+    }
+}
