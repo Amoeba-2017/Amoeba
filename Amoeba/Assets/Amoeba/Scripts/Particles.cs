5729001569df4e1b62281801e957740cfec2b33c
SirGhostler <eljosho1998@gmail.com>
2017-11-06 09:55:49 +1100

    Test fix for the particles.

---
+++ Amoeba/Assets/Amoeba/Scripts/Particles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Time from particle spawn at which the object will be destroyed.")]
+    private float DestroyTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.deltaTime == DestroyTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
