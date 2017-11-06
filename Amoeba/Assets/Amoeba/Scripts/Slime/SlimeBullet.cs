89a69300a7c601a4f7cacabab2438eea3e05a267
Jarrod-S-Design <jarrod.sevior@gmail.com>
2017-11-06 14:30:54 +1100

    - Added particles to bullets

--- Amoeba/Assets/Amoeba/Scripts/Slime/SlimeBullet.cs
+++ Amoeba/Assets/Amoeba/Scripts/Slime/SlimeBullet.cs
@@ -86,6 +86,7 @@ public class SlimeBullet : MonoBehaviour
             CreatePuddles(x);
         }
         Instantiate(BulletSplat, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+        Debug.Log("destroying this");
         Destroy(gameObject);
 
     }
