e2983f128372f5663598117883fae499a9333670
SirGhostler <eljosho1998@gmail.com>
2017-11-20 16:16:32 +1100

    Trying again.

--- Amoeba/Assets/Amoeba/Scripts/Slime/SlimeActions.cs
+++ Amoeba/Assets/Amoeba/Scripts/Slime/SlimeActions.cs
@@ -55,7 +55,7 @@ public class SlimeActions : MonoBehaviour
             // Create a Bullet object
             GameObject Bullet;
 
-            Bullet = Instantiate(projectileShot, transform.position + (rot * 2), Quaternion.FromToRotation(rot, Vector3.up));
+            Bullet = Instantiate(projectileShot, transform.position + (rot * 2), Quaternion.FromToRotation(Vector3.up, rot));
 
             if (playerController == null)
             {
